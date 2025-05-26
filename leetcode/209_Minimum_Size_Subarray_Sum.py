81938075d8e17b6e2f83530f2fd5cc85c6fd6cab
Sakshi Choudhary <sakshi2k@yahoo.co.in>
2025-05-26 18:52:41 +0200

    automated test cases + timer to compare with brut force algo

--- leetcode/209_Minimum_Size_Subarray_Sum.py
+++ leetcode/209_Minimum_Size_Subarray_Sum.py
@@ -1,7 +1,9 @@
 import sys
+from datetime import datetime as dt
 
 MAX_INT = sys.maxint if sys.version_info[0] < 3 else sys.maxsize
 
+# __optimal approach
 class Solution(object):
     def minSubArrayLen(self, target, nums):
         """
@@ -19,7 +21,6 @@ class Solution(object):
                 minCount = min(count, minCount)
                 currSum -= nums[l]
                 l += 1
-                # r = l
             r += 1
         
         return minCount if minCount != MAX_INT else 0
@@ -27,4 +28,16 @@ class Solution(object):
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.minSubArrayLen(7, [2, 3, 1, 2, 4, 3]))
+    # print(s.minSubArrayLen(7, [2, 3, 1, 2, 4, 3]))
+    
+    inputs = [[2, 3, 1, 2, 4, 3], [1, 4, 4], [1, 1, 1, 1, 1, 1, 1, 1], [1, 2, 3, 4, 5]]
+    target_vals = [7, 4, 11, 15]
+    expected_outputs = [2, 1, 0, 5]
+
+    for i in range(len(expected_outputs)):
+        time_start = dt.now()
+        assert s.minSubArrayLen(target_vals[i], inputs[i]) == expected_outputs[i], f"Failed for ${i}"
+        time_end = dt.now()
+        print(str(time_end - time_start))
+
+    print("All test cases passed!")
